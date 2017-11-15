8386d0d2268e04f937aaafc57d1058162e308a23
ablacklama <ablacklama@gmail.com>
2017-11-15 13:49:51 -0800

    initialized first measurements radar

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -2,6 +2,7 @@
 #include "tools.h"
 #include "Eigen/Dense"
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 using Eigen::MatrixXd;
@@ -97,12 +98,16 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
       /**
       Convert radar from polar to cartesian coordinates and initialize state.
       */
+		float ro = measurement_pack.raw_measurements_(0);
+		float theta = measurement_pack.raw_measurements_(1);
+		ekf_.x_(0) = ro * cos(theta);
+		ekf_.x_(1) = ro * sin(theta);
     }
     else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
       /**
       Initialize state.
       */
-		ekf_.x_(0)  = measurement_pack.raw_measurements_(0);
+		ekf_.x_(0) = measurement_pack.raw_measurements_(0);
 		ekf_.x_(1) = measurement_pack.raw_measurements_(1);
     }
 
